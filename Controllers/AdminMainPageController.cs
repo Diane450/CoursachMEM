7857bf5cd982f57c3d1c3e8b2bcf9e8940047cc3
Diane <pocket2222@mail.ru>
2024-06-01 11:34:35 +0400

    отчетик

--- Controllers/AdminMainPageController.cs
+++ Controllers/AdminMainPageController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using coursach.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OfficeOpenXml;
+using System.Xml;
 
 namespace coursach.Controllers
 {
@@ -8,6 +12,60 @@ namespace coursach.Controllers
         public IActionResult MainPage()
         {
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult DownloadExcel()
+        {
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            var dataList = GetDataFromDatabase(); // Метод для получения данных
+
+            using (var package = new ExcelPackage())
+            {
+                var worksheet = package.Workbook.Worksheets.Add("Sheet1");
+
+                // Получение типа и свойств для заголовков
+
+                string[] headers = new string [] {"Имя клиента", "Техническое задание", "Дата создания заявки", "Дата начала обработки заявки", "Статус", "Исполнитель" };
+                // Заголовки столбцов
+                for (int col = 1; col <= headers.Length; col++)
+                {
+                    worksheet.Cells[1, col].Value = headers[col - 1];
+                    worksheet.Cells[1, col].Style.Font.Bold = true;
+                    worksheet.Cells[1, col].Style.Font.Color.SetColor(System.Drawing.Color.Black);
+                    worksheet.Column(col).Width = 20;
+                }
+
+
+                // Данные
+                for (int row = 0; row < dataList.Count; row++)
+                {
+                    worksheet.Cells[row + 2, 1].Value = dataList[row].FullNameClient;
+                    worksheet.Cells[row + 2, 2].Value = dataList[row].TechnicalTask;
+                    worksheet.Cells[row + 2, 3].Value = DateOnly.FromDateTime(dataList[row].CreationDate).ToString();
+                    worksheet.Cells[row + 2, 4].Value = DateOnly.FromDateTime((DateTime)dataList[row].TakeDate).ToString();
+                    worksheet.Cells[row + 2, 5].Value = dataList[row].Status.Name;
+                    worksheet.Cells[row + 2, 6].Value = dataList[row].EmployeeInf.FullName;
+                }
+
+
+                var stream = new MemoryStream();
+                package.SaveAs(stream);
+                stream.Position = 0;
+
+                string excelName = $"Отчет-{DateTime.Now.ToString("yyyyMMddHHmmssfff")}.xlsx";
+                return File(stream, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", excelName);
+            }
+        }
+
+        private List<Request> GetDataFromDatabase()
+        {
+            using (var context = new Ispr2438MageramovEmCoursachContext())
+            {
+                return context.Requests
+                    .Include(r => r.Status)
+                    .Include(r => r.EmployeeInf).ToList();
+            }
         }
     }
 }
---
+++ Helpers/Report.cs
@@ -0,0 +1,21 @@
+﻿using iTextSharp.text.pdf;
+using iTextSharp.text;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing.Imaging;
+using coursach.Models;
+using OfficeOpenXml;
+using System.Data;
+using System.Xml;
+
+namespace courash.Helpers
+{
+    public class Report
+    {
+        public Document PdfDoc { get; set; }
+        public List<Request> Data { get; set; }
+    }
+}
